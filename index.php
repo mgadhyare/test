2e3b4b2d56d1c2784d4880fb4200afebd4a41dcc
mgadhyare <63963782+mgadhyare@users.noreply.github.com>
2020-04-19 22:10:10 +0300

    added new main style sheet

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="mainstyle.css">
 </head>
 <body>
     <h1 class="text-danger"> Welcome to my home </h1>
